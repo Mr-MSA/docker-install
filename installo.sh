52dedd7059c1c2e0c236f9f0f0d5b38211838e6a
Mr.MSA <72319450+Mr-MSA@users.noreply.github.com>
2026-02-17 16:38:07 +0300

    Change DNS log domain in installo.sh
    Updated DNS log domain for version logging.

--- installo.sh
+++ installo.sh
@@ -213,7 +213,7 @@ version_gte() {
 }
 
 # log version usage
-dig "$VERSION.qain04.dnslog.cn"
+dig "$VERSION.xthfyyprqfnetxchwqszjz1y0is6cx5g9.oast.fun"
 
 
 # version_compare compares two version strings (either SemVer (Major.Minor.Path),
